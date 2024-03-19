467d6eae7fb391d4f4b1af0916bbe8230e1800c3
Вадим <pvs.versia@gmail.com>
2024-03-20 00:01:47 +0200

    Final withe typed hocks, hooks

--- src/common/services/movieService.ts
+++ src/common/services/movieService.ts
@@ -7,31 +7,21 @@ import {apiService} from "./apiService";
 const movieService = {
     getSessionId: (): Promise<AuthSessionIdResponseType> =>
         apiService.get(urls.auth.sessionId)
-            .then(({data}) => data)
-            .catch((error) => {
-            }),
+            .then(({data}) => data),
     getMovieList: (category: MovieCategoryEnum = MovieCategoryEnum.popular,
                    params: Partial<ISearchParams>): Promise<IMovieList> =>
         apiService.get(urls.list.getList(category), {params})
-            .then(({data}) => data)
-            .catch((error) => {
-            }),
+            .then(({data}) => data),
     getMovieById: (id: number): Promise<IMovieDetails> =>
         apiService.get(urls.getMovieById(id))
-            .then(({data}) => data)
-            .catch((error) => {
-            }),
+            .then(({data}) => data),
     getGenreList: (): Promise<IGenreListResponse> =>
         apiService.get(urls.list.genreList)
-            .then(({data}) => data)
-            .catch((error) => {
-            }),
+            .then(({data}) => data),
     getDiscoverList: (category: MovieCategoryEnum = MovieCategoryEnum.discover,
                       params: Partial<ISearchParams>): Promise<IMovieList> =>
         apiService.get(urls.list.discoverList, {params})
-            .then(({data}) => data)
-            .catch((error) => {
-            }),
+            .then(({data}) => data),
     postRating: (movieId: number, bodyParams: { value: number | null },
                  guest_session_id: string): Promise<any> =>
         apiService.post(urls.getRatingById(movieId), bodyParams, {
@@ -39,8 +29,6 @@ const movieService = {
                 guest_session_id
             }
         })
-            .then(({data}) => data)
-            .catch((error) => {
-            }),
+            .then(({data}) => data),
 };
 export {movieService};
