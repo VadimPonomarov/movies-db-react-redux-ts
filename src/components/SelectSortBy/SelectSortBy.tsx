2a5ed30318771bb0c1931a9d2f96e53ddec9a159
Вадим <pvs.versia@gmail.com>
2024-03-22 15:03:56 +0200

    Final with sortType

---
+++ src/components/SelectSortBy/SelectSortBy.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import {FC} from "react";
+
+import {FormControl, Select, SelectChangeEvent} from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+import _ from "lodash";
+import {useSelector} from "react-redux";
+
+import {MovieSortByEnum} from "../../common";
+import {ISearchParams} from "../../common/hocs/interfaces";
+import {commonActions, commonSelectors, useAppDispatch} from "../../storage";
+
+import {IProps} from "./interfaces";
+
+
+const SelectSortBy: FC<IProps> = () => {
+    const {sort_by} = useSelector(commonSelectors.getSearchParams);
+    const dispatch = useAppDispatch();
+
+    const handleChange: (event: SelectChangeEvent) => void =
+        (event) => {
+            dispatch(
+                commonActions.setSearchParams({
+                        sort_by: event.target.value
+                    } as ISearchParams
+                )
+            );
+        };
+
+    return (
+        <FormControl
+            sx={{m: 1}}
+            size="small"
+        >
+            <Select
+                variant={"standard"}
+                defaultValue={MovieSortByEnum["vote_average.desc"]}
+                value={sort_by}
+                onChange={handleChange}
+            >
+                {Object.keys(MovieSortByEnum)
+                    .map(item =>
+                        <MenuItem
+                            key={Object(MovieSortByEnum)[item]}
+                            value={Object(MovieSortByEnum)[item]}
+                        >
+                            {_.upperCase(item)}
+                        </MenuItem>)
+                }
+
+            </Select>
+        </FormControl>
+    );
+};
+
+export {SelectSortBy};
