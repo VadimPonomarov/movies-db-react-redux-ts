8c0c25170a4e53e643829603c9899dbfcc269d30
Вадим <pvs.versia@gmail.com>
2024-03-24 20:33:21 +0200

    With search !

--- src/components/BadgeWithCircularContainer/BadgeWithCircular.tsx
+++ src/components/BadgeWithCircularContainer/BadgeWithCircular.tsx
@@ -1,6 +1,7 @@
-import React, {FC} from "react";
+import React, {FC, useEffect, useRef, useState} from "react";
 
-import {Box, CircularProgress, Fab, FabPropsColorOverrides} from "@mui/material";
+import {Box, CircularProgress, Fab, FabPropsColorOverrides, Typography} from "@mui/material";
+import {fromEvent, merge, tap} from "rxjs";
 
 import {UseAppEffectHook} from "../../common";
 import {useAppDispatch} from "../../storage";
@@ -54,17 +55,55 @@ const BadgeWithCircular: FC<IBadgeGrProps> =
         const handleClick: () => void =
             () => {
                 handleButtonClick();
-                dispatch(movieActions.toggleActiveCardList(movieId));
+                dispatch(
+                    movieActions
+                        .toggleActiveCardList(movieId)
+                );
             };
 
+        const ref = useRef(null);
+        const [showPointer, setShowPointer] = useState<boolean>(false);
+
+
+        useEffect(() => {
+            const enter$ =
+                fromEvent(ref.current, "mouseenter")
+                    .pipe(tap(() => setShowPointer(true)));
+            const leave$ =
+                fromEvent(ref.current, "mouseleave")
+                    .pipe(tap(() => setShowPointer(false)));
+            const mouseMoveCompleted$ =
+                merge(enter$, leave$);
+            const sub = mouseMoveCompleted$.subscribe();
+
+            return () => {
+                sub.unsubscribe();
+            };
+        }, []);
+
         return (
-            <Box className={css.Badge__Box}>
-                <Box className={css.Badge__Box_Box}>
+            <Box
+                className={css.Badge__Box}
+            >
+                <Box
+                    ref={ref}
+                    className={css.Badge__Box_Box}
+                >
                     <Fab
                         color={fabColor as FabColorType}
                         sx={buttonSx}
                         onClick={handleClick}
                     >
+                        <Typography
+                            className={css.Pointer}
+                            display={
+                                showPointer ?
+                                    "block" :
+                                    "none"
+                            }
+                        >
+                            👈 Click
+                        </Typography>
                         <>
                             {!(success || loading) && valOrFunc(initial_)}
                             {loading && valOrFunc(whileLoading)}
